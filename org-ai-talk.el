9d14973f2eec09722d6149d194305bf740d26890
Robert Krahn <robert.krahn@gmail.com>
2023-03-28 00:14:57 +0200

    don't directly require whisper anymore

--- org-ai-talk.el
+++ org-ai-talk.el
@@ -23,8 +23,50 @@
 
 ;;; Code:
 
-(require 'whisper)
+(require 'greader)
 (require 'org-ai-useful)
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; recording and transcribing speech
+
+(defvar whisper--transcribing-process)
+(defvar whisper--recording-process)
+(defvar whisper--point-buffer)
+
+(defvar org-ai-talk--whisper-transcription-buffer nil
+  "The buffer into which the speech transcription is written.")
+
+(defun org-ai-talk--record-and-transcribe-speech (then-do &optional prompt)
+  "Record speech and call `THEN-DO' with the transcription as argument.
+If `PROMPT' is non-nil, display it while recording.
+
+NOTE: This requires whisper.el to be installed. Since whisper.el
+is not part of MELPA, you have to install it manually. See
+https://github.com/natrys/whisper.el for instructions."
+  (if (fboundp 'whisper--check-install-and-run)
+      (let* ((buffer (or org-ai-talk--whisper-transcription-buffer
+                         (get-buffer-create "*org-ai-talk--whisper-transcription-buffer*"))))
+        (setq whisper--point-buffer buffer)
+        (setq org-ai-talk--whisper-transcription-buffer buffer)
+        (whisper--check-install-and-run nil "whisper-start")
+        (when prompt (message prompt))
+        (while-no-input
+          (let ((key (read-key-sequence "Press any key to stop recording, cancel with C-g or ESC...")))
+            (org-ai-talk--stop-recording
+             (if (or (equal key "")
+                     (equal key "")
+                     (equal key ""))
+                 (lambda ()
+                   (message "canceled recording")
+                   (kill-buffer org-ai-talk--whisper-transcription-buffer)
+                   (setq org-ai-talk--whisper-transcription-buffer nil))
+               (lambda ()
+                 (let ((content (with-current-buffer org-ai-talk--whisper-transcription-buffer
+                                  (string-trim (buffer-string)))))
+                   (funcall then-do content))
+                 (kill-buffer org-ai-talk--whisper-transcription-buffer)
+                 (setq org-ai-talk--whisper-transcription-buffer nil)))))))
+    (error "The package whisper.el is not installed but needed for transcribing speech")))
 
 (defun org-ai-talk--stop-recording (then-do)
   "Force current recording to stop and call `THEN-DO'."
@@ -42,85 +84,8 @@
        (funcall then-do)))
    then-do))
 
-(defvar org-ai--whisper-transcription-buffer nil
-  "The buffer into which the speech transcription is written.")
-
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-(defun org-ai-keyboard-quit ()
-  "If there is currently a running request, cancel it."
-  (interactive)
-  (condition-case _
-      (when org-ai--current-request-buffer
-        (org-ai-interrupt-current-request))
-    (error nil)))
-
-(defun org-ai-talk--install-keyboard-quit-advice ()
-  "Cancel current request when `keyboard-quit' is called."
-  (unless (advice-member-p #'org-ai-keyboard-quit 'keyboard-quit)
-    (advice-add 'keyboard-quit :before #'org-ai-keyboard-quit)))
-
-(defun org-ai-talk--uninstall-keyboard-quit-advice ()
-  "Remove the advice that cancels current request when `keyboard-quit' is called."
-  (advice-remove 'keyboard-quit #'org-ai-keyboard-quit))
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-(defun org-ai-talk--speech-input (then-do &optional prompt)
-  "Record speech and call `THEN-DO' with the transcription as argument.
-If `PROMPT' is non-nil, display it while recording."
-  (lexical-let ((then-do then-do))
-    (let* ((buffer (or org-ai--whisper-transcription-buffer
-                       (get-buffer-create "*org-ai--whisper-transcription-buffer*")))
-           (whisper--point-buffer buffer))
-      (setq org-ai--whisper-transcription-buffer buffer)
-      (whisper--check-install-and-run nil "whisper-start")
-      (when prompt (message prompt))
-      (while-no-input
-        (let ((key (read-key-sequence "Press any key to stop recording, cancel with C-g or ESC...")))
-          (org-ai-talk--stop-recording
-           (if (or (equal key "")
-                   (equal key "")
-                   (equal key ""))
-               (lambda ()
-                 (message "canceled recording")
-                 (kill-buffer org-ai--whisper-transcription-buffer)
-                 (setq org-ai--whisper-transcription-buffer nil))
-             (lambda ()
-               (let ((content (with-current-buffer org-ai--whisper-transcription-buffer
-                                (string-trim (buffer-string)))))
-                 (funcall then-do content))
-               (kill-buffer org-ai--whisper-transcription-buffer)
-               (setq org-ai--whisper-transcription-buffer nil)))))))))
-
-(defun org-ai-talk-everywhere (&optional output-buffer)
-  "The same as `ORG-AI-PROMPT' but uses speech input.
-If `OUTPUT-BUFFER' is non-nil, insert the response there."
-  (interactive)
-  (lexical-let ((output-buffer (or (current-buffer) output-buffer)))
-    (org-ai-talk--speech-input (lambda (spoken-text)
-                                    (with-current-buffer output-buffer
-                                      (insert spoken-text)
-                                      (insert "\n\n"))
-                                    (org-ai-prompt spoken-text :output-buffer output-buffer))
-                                  "Say something then press any key...")))
-
-(defun org-ai-talk-in-org (&optional output-buffer)
-  "The same as `ORG-AI-PROMPT' but uses speech input.
-If `OUTPUT-BUFFER' is non-nil, insert the response there."
-  (interactive)
-  (if-let* ((context (org-ai-special-block))
-            (content-end (org-element-property :contents-end context)))
-      (progn
-        (goto-char content-end)
-        (backward-char 1)
-        (org-ai-talk--speech-input (lambda (content)
-                                        (insert content)
-                                        (org-ai-complete-block))
-                                      "Say something then press any key..."))
-   (org-ai-talk-everywhere)))
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; synthesizing speech
 
 (defcustom org-ai-talk-use-greader
   (not (string-equal system-type "darwin"))
@@ -184,28 +149,23 @@ If `CALLBACK' is non-nil, call it when done."
   "Read the region from `FROM' to `TO'.
 If `CALLBACK' is non-nil, call it when done.
 Uses greader / espeak, should work on all platforms where espeak is installed."
-  (lexical-let ((buf (current-buffer))
-                (callback callback)
-                (to to))
-    (if-let ((text (buffer-substring-no-properties from to)))
-        (org-ai-talk-use-greader
-         (greader-read-asynchronous text)
-         (setq org-ai-talk--reading-process greader-synth-process)
-         (org-ai-talk--wait-for-greader callback))
-      (warn "no sentence"))))
+  (if-let ((text (buffer-substring-no-properties from to)))
+      (progn
+       (greader-read-asynchronous text)
+       (setq org-ai-talk--reading-process greader-synth-process)
+       (org-ai-talk--wait-for-greader callback))
+    (warn "no sentence")))
 
 (defun org-ai-talk--read-region-macos (from to &optional callback)
   "Read the region from `FROM' to `TO'.
 If `CALLBACK' is non-nil, call it when done.
 For MacOS only, uses the `say' cli utility."
-  (lexical-let ((buf (current-buffer))
-                (callback callback)
-                (to to))
+  (let ((buf (current-buffer)))
     (if-let ((text (buffer-substring-no-properties from to)))
         (setq org-ai-talk--reading-process
               (make-process :name "*org-ai-talker*"
                             :command (list "say" text)
-                            :sentinel (lambda (&optional process event)
+                            :sentinel (lambda (&optional _process event)
                                         (when (string= event "finished\n")
                                           (setq org-ai-talk--reading-process nil)
                                           (with-current-buffer buf (goto-char to))
@@ -230,17 +190,6 @@ If `CALLBACK' is non-nil, call it when done."
 If `CALLBACK' is non-nil, call it when done."
   (org-ai-talk-read-region (point) (save-excursion (forward-sentence) (point)) callback))
 
-(comment
-  (progn
-   (setq org-ai-talk--current-insertion-point 0)
-   (setq org-ai-talk--is-done nil)
-   (let ((text "")) (org-ai-talk--speak-inserted-text 'role text 0 (length text)))
-   (let ((text "hello world")) (org-ai-talk--speak-inserted-text 'text text 0 (length text)))
-   (let ((text " how. are you?")) (org-ai-talk--speak-inserted-text 'text text 0 (length text)))
-   (org-ai-talk--speak-inserted-text 'end "" 0 0))
-
-  (org-ai-talk-read-region (region-beginning) (region-end)))
-
 (defun org-ai-talk-stop ()
   "Stop recording and synthesis."
   (interactive)
@@ -255,6 +204,38 @@ If `CALLBACK' is non-nil, call it when done."
   (setq org-ai-talk--reading-process nil)
   (setq org-ai-talk--current-insertion-point 0)
   (setq org-ai-talk--is-done t))
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; the actual useful commands
+
+(defun org-ai-talk-everywhere (&optional output-buffer)
+  "The same as `ORG-AI-PROMPT' but uses speech input.
+If `OUTPUT-BUFFER' is non-nil, insert the response there."
+  (interactive)
+  (let ((output-buffer (or (current-buffer) output-buffer)))
+    (org-ai-talk--record-and-transcribe-speech (lambda (spoken-text)
+                                                 (with-current-buffer output-buffer
+                                                   (insert spoken-text)
+                                                   (insert "\n\n"))
+                                                 (org-ai-prompt spoken-text :output-buffer output-buffer))
+                                               "Say something then press any key...")))
+
+(defun org-ai-talk-in-org ()
+  "The same as `ORG-AI-PROMPT' but uses speech input."
+  (interactive)
+  (if-let* ((context (org-ai-special-block))
+            (content-end (org-element-property :contents-end context)))
+      (progn
+        (goto-char content-end)
+        (backward-char 1)
+        (org-ai-talk--record-and-transcribe-speech
+         (lambda (content)
+           (insert content)
+           ;; need check here to make linter happy
+           (when (fboundp 'org-ai-complete-block)
+             (org-ai-complete-block)))
+         "Say something then press any key..."))
+    (org-ai-talk-everywhere)))
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
