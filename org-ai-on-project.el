31921a711ef5a99078b0c0b954ccc2e8d50dfcde
Robert Krahn <robert.krahn@gmail.com>
2023-04-16 01:03:38 +0200

    run the org-ai-on-project prompt and extract files
    files are extracted using `org-ai-on-project--extract-files-and-code-blocks` and
    then those files are created alongside the originals with a .orgai__ prefix

--- org-ai-on-project.el
+++ org-ai-on-project.el
@@ -24,6 +24,7 @@
 
 (require 'cl-lib)
 (require 'widget)
+(require 'spinner)
 (require 'org-ai-useful)
 
 (eval-when-compile
@@ -48,12 +49,29 @@
   region
   chosen)
 
+(cl-defstruct org-ai-on-project--request-in-progress
+  "State of the request in progress."
+  state
+  start-pos
+  url-response-buffer
+  spinner)
+
 (defvar-local org-ai-on-project--current-state nil
   "Current state of the on-project buffer.")
 
 (defvar org-ai-on-project--buffer-name "*org-ai-on-project*"
   "Name of the on-project buffer.")
 
+(defvar org-ai-on-project--current-request-in-progress nil "")
+
+(defun my-toggle-spinner ()
+  (interactive)
+  (if my-spinner
+      (progn
+        (spinner-stop my-spinner)
+        (setq my-spinner nil))
+    (setq my-spinner (spinner-create 'progress-bar t))
+    (spinner-start my-spinner)))
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ;; helpers
@@ -67,8 +85,11 @@
 (defun org-ai-on-project--find-files (base-dir patterns)
   (let* ((default-directory base-dir)
          (patterns (string-split patterns " " t))
-         (known-project-files (and (fboundp #'projectile-current-project-files)
-                                   (projectile-current-project-files))))
+         (known-project-files (or
+                               (and (boundp 'projectile-mode)
+                                    projectile-mode
+                                    (fboundp #'projectile-current-project-files)
+                                    (projectile-current-project-files)))))
 
     ;; This is quite a hack here:
     ;; If we have a pattern like "**/*js", it will only search in subdirectories,
@@ -87,6 +108,7 @@
              for pattern in patterns
              do (cl-loop for file in (file-expand-wildcards pattern)
                          when (and (file-regular-p file)
+                                   (not (string-match-p "__org-ai__" file))
                                    (or (not known-project-files)
                                        (member file known-project-files)))
                          do (push (make-org-ai-on-project--file
@@ -114,6 +136,46 @@
        (setq end (point))
        (buffer-substring-no-properties start end))))
 
+(defun org-ai-on-project--extract-files-and-code-blocks (&optional start end)
+  "Expects that the current buffer shows files and code.
+This should be in the form:
+
+file-name
+```
+code
+```
+...
+
+`START' and `END' are optional and can be used to limit the
+region.
+
+Will return a hash table with the file names as keys and the
+code as values."
+  (let ((content (buffer-substring-no-properties (or start (point-min))
+                                                 (or end (point-max))))
+                 (result (make-hash-table :test 'equal)))
+        (with-temp-buffer
+          (insert content)
+          (beginning-of-buffer)
+          (cl-loop while (search-forward "```" nil t)
+                   ;; sanity check, is this a code block, not embedded?
+                   when (= (current-column) 3)
+                   do (let (file-name file-content)
+                        ;; we are at the end of the code block start
+                        ;; get the file name
+                        (beginning-of-line)
+                        (forward-line -1)
+                        (setq file-name (buffer-substring-no-properties
+                                         (line-beginning-position)
+                                         (line-end-position)))
+                        (forward-line 2)
+                        (let ((content-start (point)))
+                          (search-forward "```")
+                          (setq file-content (buffer-substring-no-properties
+                                              content-start
+                                              (line-beginning-position))))
+                        (puthash file-name file-content result)))
+          result)))
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ;; helper mode and functions for selecting regions in files
@@ -175,6 +237,7 @@ FILE is of type `org-ai-on-project--file`. It is not a string!"
      (switch-to-buffer (get-buffer-create org-ai-on-project--buffer-name))
      (kill-all-local-variables)
      (with-current-buffer org-ai-on-project--buffer-name
+       (toggle-truncate-lines -1)
        ,@body
        (use-local-map widget-keymap)
        (widget-setup)
@@ -302,13 +365,32 @@ Here are the files:"
   :type 'string
   :group 'org-ai-on-project)
 
+(defcustom org-ai-on-project-default-modify-prompt
+  "Now modify the code according to the request. Show it in the same format, file name followed by content. Leave out any files that you did not modify. You can add new files or split existing ones if necessary. Do not at any explanation whatsoever."
+  "Default prompt for org-ai-on-project."
+  :type 'string
+  :group 'org-ai-on-project)
+
+(defcustom org-ai-on-project-default-request-prompt
+  "Now answer the request using the file contents."
+  "Default prompt for org-ai-on-project."
+  :type 'string
+  :group 'org-ai-on-project)
+
+(defcustom org-ai-on-project-use-stream t
+  "If non-nil, use streaming to get the result.")
+
 (defun org-ai-on-project--run (state)
   ""
   (let ((buf (get-buffer-create "*org-ai-on-project-result*"))
+        (final-instruction (if (org-ai-on-project--state-modify-code state)
+                               org-ai-on-project-default-modify-prompt
+                             org-ai-on-project-default-request-prompt))
         (files (cl-loop for file in (org-ai-on-project--state-files state)
                         when (org-ai-on-project--file-chosen file)
                         collect file)))
     (with-current-buffer buf
+      (toggle-truncate-lines -1)
       (erase-buffer)
       (insert (org-ai-on-project--state-prompt state))
       (insert "\n")
@@ -319,11 +401,73 @@ Here are the files:"
                     (insert "```\n")
                     (insert content)
                     (insert "```\n\n")))
-      (insert "Now answer the request!\n\n"))
+      (insert final-instruction "\n\n")
     (switch-to-buffer buf)
+    (recenter-top-bottom 1)
 
-    (let ((prompt (buffer-string)))
-      (org-ai-prompt prompt :output-buffer (current-buffer) :callback (lambda (response) (message "done"))))))
+    (let* ((prompt (buffer-string))
+           (buf (current-buffer))
+           (response-buffer (if org-ai-on-project-use-stream
+                                (org-ai-prompt prompt
+                                               :output-buffer (current-buffer)
+                                               :callback (lambda ()
+                                                           (with-current-buffer buf
+                                                             (org-ai-on-project--request-cleanup))))
+                              (org-ai-chat-request
+                               :messages (org-ai--collect-chat-messages prompt)
+                               :model org-ai-default-chat-model
+                               :callback (lambda (content role usage)
+                                           (with-current-buffer buf
+                                             (save-excursion
+                                               (insert content))
+                                             (org-ai-on-project--request-cleanup)))))))
+
+      (setq org-ai-on-project--current-request-in-progress
+            (make-org-ai-on-project--request-in-progress
+             :state state
+             :start-pos (point)
+             :spinner (spinner-start 'progress-bar)
+             :url-response-buffer response-buffer))))))
+
+(defun org-ai-on-project--request-cleanup ()
+  ""
+  (when-let (current org-ai-on-project--current-request-in-progress)
+    (spinner-stop)
+    ;; (display-buffer (org-ai-on-project--request-in-progress-url-response-buffer current))
+    (unless org-ai-on-project-use-stream
+      (kill-buffer (org-ai-on-project--request-in-progress-url-response-buffer current)))
+    ;;(setq org-ai-on-project--current-request-in-progress nil)
+    ))
+
+
+
+(comment
+  org-ai-on-project--current-request-in-progress
+
+  (with-current-buffer "*org-ai-on-project-result*"
+    (let ((pos (org-ai-on-project--request-in-progress-start-pos org-ai-on-project--current-request-in-progress)))
+      (goto-char pos)
+      (let ((modified (org-ai-on-project--extract-files-and-code-blocks))
+            (original-and-modified-files (make-hash-table :test 'equal)))
+        (cl-loop for key being the hash-keys of modified
+                 using (hash-values value)
+                 ;; for each file, create a __org-ai__ file that contains the modified content
+                 do (let ((modified-file (replace-regexp-in-string "\\(.*/\\)?\\(.*\\)" "\\1__org-ai__\\2" key)))
+                      (puthash key modified-file original-and-modified-files)
+                      (with-temp-file modified-file
+                        (insert value))))
+        (setq xxx original-and-modified-files))))
+
+
+  (hash-table-keys xxx)
+  (gethash "test.js" xxx)
+
+  (ediff-merge-files "test.js" "__org-ai__test.js")
+  (setq file "foo/bar/baz.org")
+  (setq file "baz.org"))
+
+
+
 
 ;;;###autoload
 (defun org-ai-on-project (&optional base-dir)
