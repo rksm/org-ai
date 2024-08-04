1b9c389f6befb71ef769d1d52b7d3f0c8598dbfc
Jason Fry <small.job8148@fastmail.com>
2024-08-04 19:30:04 -0400

    Add ability to send image as a link
    https://github.com/rksm/org-ai/issues/122

--- org-ai-openai-image-query.el
+++ org-ai-openai-image-query.el
@@ -1,4 +1,4 @@
-;;; org-ai-openai-image-query.el --- Send base64 image and question to OpenAI -*- lexical-binding: t; -*-
+;;; org-ai-openai-image-query.el --- Send base64 or URL image and question to OpenAI -*- lexical-binding: t; -*-
 
 ;;; Code:
 
@@ -7,28 +7,24 @@
 (defcustom org-ai-image-query-model "gpt-4o-mini"
   "The model to use for image queries."
   :group 'org-ai
-  :type '(choice (const :tag "GPT-4o-mini" "gpt-4o-mini")
-          (const :tag "GPT-4o" "gpt-4o")
-          (const :tag "GPT-4-turbo" "gpt-4-turbo")))
+  :type '(choice (const :tag "gpt-4o-mini" "gpt-4o-mini")
+          (const :tag "gpt-4o" "gpt-4o")
+          (const :tag "gpt-4-turbo" "gpt-4-turbo")))
 
 (defvar org-ai-openai-image-query-endpoint "https://api.openai.com/v1/chat/completions"
   "Endpoint for querying images with OpenAI.")
 
-(defun org-ai--send-image-query (base64-image question callback)
-  "Send BASE64-IMAGE and QUESTION to the OpenAI API.
+(defun org-ai--send-image-query (image-content question callback)
+  "Send IMAGE-CONTENT and QUESTION to the OpenAI API.
 Calls CALLBACK with the response."
-  (let* ((url-request-extra-headers `(("Authorization" . ,(string-join `("Bearer" ,(org-ai--openai-get-token)) " "))
-                                      ("Content-Type" . "application/json")))
-         (url-request-method "POST")
-         (url-request-data (json-encode `(("model" . ,org-ai-image-query-model)
-                                          ("messages" . [((role . "user")
-                                                          (content . [
-                                                                      ((type . "text")
-                                                                       (text . ,question))
-                                                                      ((type . "image_url")
-                                                                       (image_url . ((url . ,(concat "data:image/jpeg;base64," base64-image)))))])
-                                                          )])
-                                          ("max_tokens" . 300)))))
+  (let ((url-request-extra-headers `(("Authorization" . ,(string-join `("Bearer" ,(org-ai--openai-get-token)) " "))
+                                     ("Content-Type" . "application/json")))
+        (url-request-method "POST")
+        (url-request-data (json-encode `(("model" . ,org-ai-image-query-model)
+                                         ("messages" . [(("role" . "user")
+                                                         ("content" . [((type . "text") (text . ,question))
+                                                                       ,image-content]))])
+                                         ("max_tokens" . 300)))))
     (url-retrieve
      org-ai-openai-image-query-endpoint
      (lambda (_status)
@@ -38,20 +34,49 @@ Calls CALLBACK with the response."
              (funcall callback response)
            (message "Failed to get a valid response from OpenAI API.")))))))
 
+(defun org-ai--send-base64-image-query (base64-image question callback)
+  "Send a BASE64-IMAGE and QUESTION to the OpenAI API.
+Calls CALLBACK with the response."
+  (org-ai--send-image-query
+   `((type . "image_url")
+     (image_url . (("url" . ,(concat "data:image/jpeg;base64," base64-image)))))
+   question callback))
+
+(defun org-ai--send-url-image-query (image-url question callback)
+  "Send an IMAGE-URL and QUESTION to the OpenAI API.
+Calls CALLBACK with the response."
+  (org-ai--send-image-query
+   `((type . "image_url")
+     (image_url . (("url" . ,image-url))))
+   question callback))
+
+(defun org-ai--handle-openai-response (response)
+  "Handle the RESPONSE from OpenAI API."
+  (let ((choices (cdr (assq 'choices response))))
+    (if choices
+        (message "OpenAI API response: %S" choices)
+      (message "OpenAI API returned an unexpected response: %S" response))))
+
+(defun org-ai--get-user-input ()
+  "Get input from the user."
+  (interactive)
+  (list (read-string "Image file path or URL: ")
+        (read-string "Question: ")))
+
 ;;;###autoload
-(defun org-ai-query-image (image-path question)
-  "Query OpenAI API with a BASE64 encoded IMAGE-PATH and a QUESTION."
-  (interactive "fImage file path: \nsQuestion: ")
-  (let ((base64-image (with-temp-buffer
-                        (insert-file-contents-literally image-path)
-                        (base64-encode-region (point-min) (point-max))
-                        (buffer-string))))
-    (org-ai--send-image-query base64-image question
-                              (lambda (response)
-                                (let ((choices (cdr (assq 'choices response))))
-                                  (if choices
-                                      (message "OpenAI API response: %S" choices)
-                                    (message "OpenAI API returned an unexpected response: %S" response)))))))
+(defun org-ai-query-image ()
+  "Query OpenAI API with a BASE64 encoded image path or URL and a QUESTION."
+  (interactive)
+  (let* ((inputs (org-ai--get-user-input))
+         (image-path-or-url (car inputs))
+         (question (cadr inputs)))
+    (if (string-match-p "^https?://" image-path-or-url)
+        (org-ai--send-url-image-query image-path-or-url question #'org-ai--handle-openai-response)
+      (let ((base64-image (with-temp-buffer
+                            (insert-file-contents-literally image-path-or-url)
+                            (base64-encode-region (point-min) (point-max))
+                            (buffer-string))))
+        (org-ai--send-base64-image-query base64-image question #'org-ai--handle-openai-response)))))
 
 (provide 'org-ai-openai-image-query)
 
