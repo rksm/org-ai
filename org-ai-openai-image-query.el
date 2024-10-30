a6319fde177030abe792e5fa648b3382b3c51c77
Jason Fry <small.job8148@fastmail.com>
2024-10-30 12:44:34 -0400

    prefill input when cursor is on a filename or link
    https://github.com/rksm/org-ai/issues/122

--- org-ai-openai-image-query.el
+++ org-ai-openai-image-query.el
@@ -2,7 +2,6 @@
 
 ;;; Code:
 
-;; TODO use file path or url at point as default
 (require 'org-ai-openai)
 
 (defcustom org-ai-image-query-model "gpt-4o-mini"
@@ -79,16 +78,10 @@ Calls CALLBACK with the response."
 ;;;###autoload
 (defun org-ai-query-image ()
   "Query OpenAI API with a BASE64 encoded image path or URL and a QUESTION."
-  (interactive)
-  (let ((image-path-or-url (org-ai--get-image-path-or-url))
-        (question (org-ai--get-question)))
-    (if (string-match-p "^https?://" image-path-or-url)
-        (org-ai--send-url-image-query image-path-or-url question #'org-ai--handle-openai-response)
-      (let ((base64-image (with-temp-buffer
-                            (insert-file-contents-literally image-path-or-url)
-                            (base64-encode-region (point-min) (point-max))
-                            (buffer-string))))
-        (org-ai--send-base64-image-query base64-image question #'org-ai--handle-openai-response)))))
+  (interactive (list
+                (read-file-name "Image file path: " nil nil nil (or (thing-at-point 'existing-filename)
+                                                                    (thing-at-point 'url)))
+                (read-string "Question: " nil 'minibuffer-history))))
 
 (provide 'org-ai-openai-image-query)
 
